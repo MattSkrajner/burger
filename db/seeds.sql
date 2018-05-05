b25622a1cc53b48ae9f90977f7801910881d5fdc
MattSkrajner <matthew.skrajner@gmail.com>
2018-05-05 04:35:48 -0400

    buggy mess

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name)
+VALUES ("Double Cheeseburger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Veggie Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Chipotle BBQ Burger");
